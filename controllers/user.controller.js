5a275fcbe823de7ff00985e63182759b2e0a9e75
Aime Malaika <aimemalaika1995@gmail.com>
2019-10-21 15:28:33 +0200

    [Finishes #169225213] fixed registration saving of data (registration work with all validation)

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 import localStorage from 'localStorage';
 import bcrypt from 'bcrypt';
 
@@ -12,12 +13,28 @@ exports.getLoginAuth = (req, res, next) => {
 exports.getRegisterAuth = (req, res, next) => {
   const validation = new Validate();
   const values = req.body;
+  let idUser;
   const usersRecord = JSON.parse(localStorage.getItem('users')) || [];
   const passed = validation.check(userModule.UserRegitration, values, usersRecord);
   if (passed === true) {
+    if (usersRecord.length === 0) {
+      idUser = 1;
+      console.log(usersRecord);
+    } else {
+      const maxId = (array, prop) => {
+        let max;
+        for (let i = 0; i < array.length; i += 1) {
+          if (!max || parseInt(array[i][prop]) > parseInt(max[prop])) { max = array[i]; }
+        }
+        return max.id + 1;
+      };
+
+      idUser = maxId(usersRecord, 'id');
+    }
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(values.Password, salt);
     usersRecord.push({
+      id: idUser,
       First_Name: values.First_Name,
       Last_Name: values.Last_Name,
       Email: values.Email,
