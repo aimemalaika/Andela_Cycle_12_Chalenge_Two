f7067b0615e0055a2fa7117501115ad65b2e67a2
Aime Malaika <aimemalaika1995@gmail.com>
2019-11-04 15:41:01 +0200

    [Delivers #169531964] ignoring coverage and adding query

--- services/queries.js
+++ services/queries.js
@@ -8,6 +8,8 @@ const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const users = {
   insertUser: `INSERT INTO users( first_name, last_name, email , password) VALUES($1 ,$2, $3, $4) RETURNING *`,
   isUserExist: `SELECT * FROM users WHERE email = $1`,
+  updateUser: `UPDATE users SET first_name=$1, last_name=$2, email=$3 WHERE ID=$4`,
+  updatePassword: `UPDATE users SET password=$1 WHERE ID=$2`,
 };
 const entries = {
   isMentorshipExist: `SELECT * FROM stories WHERE auther= $1`,
