2cf344cd823c1020d5a78e0a00a98d997bf16769
Aime Malaika <aimemalaika1995@gmail.com>
2019-11-04 20:56:58 +0200

    [Finishes #169531964] Added database setting and table creations plus connection script

---
+++ services/createTables.js
@@ -0,0 +1,24 @@
+import { pool } from './config';
+
+const makeTables = async () => {
+  const createUsers = `CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY UNIQUE,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+  );`;
+
+  const createStory = `CREATE TABLE IF NOT EXISTS stories(
+        id SERIAL PRIMARY KEY UNIQUE,
+        subject TEXT NOT NULL,
+        content TEXT NOT NULL,
+        createdOn TEXT NOT NULL,
+        auther INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+    );`;
+
+  await pool.query(createUsers);
+  await pool.query(createStory);
+};
+
+makeTables();
---
+++ services/dropTable.js
@@ -0,0 +1,14 @@
+import { pool } from './config';
+
+const dropTable = [
+  `DROP TABLE IF EXISTS stories CASCADE`,
+  `DROP TABLE IF EXISTS users CASCADE`,
+];
+
+const dropTables = async () => {
+  for (const i of dropTable) {
+    await pool.query(i);
+  }
+};
+
+dropTables();
