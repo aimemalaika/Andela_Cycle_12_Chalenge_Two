289420c9d6841f5a3ee302c23e0a30e423f6d726
Aime Malaika <aimemalaika1995@gmail.com>
2019-11-04 21:26:37 +0200

    [Delivers #169531964] Adding sql queries that will be used in the api

---
+++ services/queries.js
@@ -0,0 +1,16 @@
+import { pool } from './config';
+
+const users = {
+  insertUser: `INSERT INTO users( first_name, last_name, email , password) VALUES($1 ,$2, $3, $4) RETURNING *`,
+  isUserExist: `SELECT * FROM users WHERE email = $1`,
+  updateUser: `UPDATE users SET first_name=$1, last_name=$2, email=$3 WHERE ID=$4`,
+  updatePassword: `UPDATE users SET password=$1 WHERE ID=$2`,
+};
+const entries = {
+  getAllStories: `SELECT * FROM stories WHERE auther=$1`,
+  getOneStory: `SELECT * FROM stories WHERE auther=$1 AND id=$2`,
+  deleteStory: `DELETE FROM stories WHERE auther=$1 AND id=$2`,
+  updateStory: `UPDATE stories SET subject=$1,content=$2 WHERE auther=$1 AND id=$2`,
+};
+
+export default { users, entries };
