a3e6783c0dd34dae105e45b161185593c06d3b55
Aime Malaika <aimemalaika1995@gmail.com>
2019-11-06 03:39:03 +0200

    [Finishes #169573081] done with delete story controller

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -30,7 +30,7 @@ exports.getOneStory = async (req, res) => {
     }
     return res.status(200).json({
       status: 200,
-      data: isPostExist.rows,
+      data: isPostExist.rows[0],
     });
   } catch (err) {
     return res.status(400).json({ status: 400, error: err.message });
@@ -52,39 +52,20 @@ exports.getAllStories = async (req, res) => {
   }
 };
 
-exports.deleteStory = (req, res) => {
-  const { storyId } = req.params;
-  const auther = req.id;
-  const storyRecord = JSON.parse(localStorage.getItem('stories')) || [];
-  if (storyRecord.length > 0) {
-    const found = storyRecord.find((storydata) => storydata.id === parseInt(storyId));
-    if (typeof (found) !== 'undefined') {
-      if (found.Auther === parseInt(auther)) {
-        const key = storyRecord.indexOf(found);
-        delete storyRecord[key];
-        const data = storyRecord.filter((x) => x !== null);
-        localStorage.setItem('stories', JSON.stringify(data));
-        res.status(204).json({
-          status: 204,
-          message: 'entry successfully deleted',
-        });
-      } else {
-        res.status(403).json({
-          status: 403,
-          message: 'trying to get post that are not yours',
-        });
-      }
-    } else {
-      res.status(404).json({
-        status: 404,
-        message: 'story not found',
-      });
+exports.deleteStory = async (req, res) => {
+  const isPostExist = await config.executeQuery(queries.entries.getOneStory, [req.id, req.params.storyId]);
+  try {
+    if (isPostExist.rowCount === 0) {
+      return res.status(409).json({ status: 409, error: "no topic found" });
     }
-  } else {
-    res.status(404).json({
-      status: 404,
-      message: 'story not found',
+    const { id } = isPostExist.rows[0];
+    await config.executeQuery(queries.entries.deleteStory, [id]);
+    return res.status(204).json({
+      status: 204,
+      message: 'entry deleted succesfully',
     });
+  } catch (err) {
+    return res.status(400).json({ status: 400, error: err.message });
   }
 };
 
